1e7c4dc9d1f1a43399490f2181740b191357155e
Daniel Melo <daniel.melo42@outlook.com>
2022-12-31 11:29:39 -0700

    setting up hilt AppModule and the NoteApplication component

--- app/src/main/java/com/study/jetnotes/MainActivity.kt
+++ app/src/main/java/com/study/jetnotes/MainActivity.kt
@@ -13,7 +13,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.study.jetnotes.navigation.NotesNavigation
 import com.study.jetnotes.ui.theme.JetNotesTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/study/jetnotes/NoteApplication.kt
@@ -0,0 +1,10 @@
+package com.study.jetnotes
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class NoteApplication : Application() {
+
+}
