90ed2ac14aae47066ec2c83ce97fe018b9f2ade4
Daniel Melo <daniel.melo42@outlook.com>
2022-12-31 12:40:31 -0700

    NoteDatabase and NoteDatabaseDao implemented

---
+++ app/src/main/java/com/study/jetnotes/data/NoteDatabase.kt
@@ -0,0 +1,10 @@
+package com.study.jetnotes.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.study.jetnotes.model.Note
+
+@Database(entities = [Note::class], version = 1, exportSchema = false)
+abstract class NoteDatabase : RoomDatabase() {
+    abstract fun noteDao() : NoteDatabaseDao
+}
---
+++ app/src/main/java/com/study/jetnotes/data/NoteDatabaseDao.kt
@@ -0,0 +1,31 @@
+package com.study.jetnotes.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.study.jetnotes.model.Note
+
+@Dao
+interface NoteDatabaseDao {
+
+    @Query("SELECT * FROM notes_tbl")
+    fun getNotes(): List<Note>
+
+    @Query("SELECT * FROM notes_tbl WHERE id = :id")
+    fun getNoteById(id: String): Note
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(note: Note)
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    fun update(note: Note)
+
+    @Query("DELETE FROM notes_tbl")
+    fun deleteAllNotes()
+
+    @Delete
+    fun deleteNote(note: Note)
+}
