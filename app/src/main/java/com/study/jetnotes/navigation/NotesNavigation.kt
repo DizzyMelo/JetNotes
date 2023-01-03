bae8c126a817f5243ee989ad63b71304d1f9c146
Daniel Melo <daniel.melo42@outlook.com>
2023-01-02 18:39:35 -0700

    removed navigation as app has only one screen

--- app/src/main/java/com/study/jetnotes/navigation/NotesNavigation.kt
+++ app/src/main/java/com/study/jetnotes/navigation/NotesNavigation.kt
@@ -1,13 +1,9 @@
 package com.study.jetnotes.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.study.jetnotes.screens.home.NoteScreen
-import com.study.jetnotes.screens.home.NoteViewModel
 
 @Composable
 fun NotesNavigation() {
