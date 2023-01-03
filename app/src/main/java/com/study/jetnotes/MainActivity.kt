8ca96c48f3c113c69967b001b8cf8eea0e88d990
Daniel Melo <daniel.melo42@outlook.com>
2023-01-02 18:40:15 -0700

    removed unnecessary imports

--- app/src/main/java/com/study/jetnotes/MainActivity.kt
+++ app/src/main/java/com/study/jetnotes/MainActivity.kt
@@ -6,14 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.ComposeCompilerApi
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.study.jetnotes.navigation.NotesNavigation
 import com.study.jetnotes.screens.home.NoteScreen
 import com.study.jetnotes.screens.home.NoteViewModel
 import com.study.jetnotes.ui.theme.JetNotesTheme
